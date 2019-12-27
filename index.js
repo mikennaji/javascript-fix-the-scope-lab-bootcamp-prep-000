5e43de15563703bbc10d4f96b20346a0195a3cb2
Michael Nnaji <mnnaji3@uwo.ca>
2019-12-27 11:51:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,15 @@ var funkyFunction = function() {
   }
 }
 
+var funkyFunction =function outerFunction() {
+  var innerVariable = "FUNKY!";
+ 
+  return function innerScope() {
+     return innerVariable;
+  }
+}
+
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
-var theFunk = funkyFunction()
+var theFunk = funkyFunction
